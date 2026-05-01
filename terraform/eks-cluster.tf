2d91e12d9e12113bf11a536b5f082cad5e330d06
risbern21 <risbernfernandes21@gmail.com>
2026-05-01 10:56:53 +0530

    fixed ig

--- terraform/eks-cluster.tf
+++ terraform/eks-cluster.tf
@@ -1,23 +1,18 @@
 module "eks" {
   source  = "terraform-aws-modules/eks/aws"
-  version = "20.31.6"
+  version = "21.19.0"
 
-  cluster_name    = local.cluster_name
-  cluster_version = "1.32"
+  name               = local.cluster_name
+  kubernetes_version = "1.32"
 
-  vpc_id                         = module.vpc.vpc_id
-  subnet_ids                     = module.vpc.private_subnets
-  cluster_endpoint_public_access = true
-
-  eks_managed_node_group_defaults = {
-    ami_type = "AL2_x86_64"
-
-  }
+  vpc_id                  = module.vpc.vpc_id
+  subnet_ids              = module.vpc.private_subnets
+  endpoint_public_access  = true
 
   eks_managed_node_groups = {
     one = {
-      name = "node-group-1"
-
+      name           = "node-group-1"
+      ami_type       = "AL2023_x86_64_STANDARD"
       instance_types = ["t3.small"]
 
       min_size     = 1
@@ -26,8 +21,8 @@ module "eks" {
     }
 
     two = {
-      name = "node-group-2"
-
+      name           = "node-group-2"
+      ami_type       = "AL2023_x86_64_STANDARD"
       instance_types = ["t3.small"]
 
       min_size     = 1
